e1ae6f53d65e14f20c16b004eaec0f377fbe2c80
cardeol <cardeol@gmail.com>
2013-12-31 14:53:00 +0000

    Update class.luasapi.php

--- class.luasapi.php
+++ class.luasapi.php
@@ -76,12 +76,14 @@ EEE;
 
 	private function get_url($url) {
 	    $ch = curl_init();
-	      curl_setopt($ch, CURLOPT_HEADER, 0);
-	      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	      curl_setopt($ch, CURLOPT_URL, $url);
-	      $data = curl_exec($ch);
-	      curl_close($ch);
-	      return $data;
+	    curl_setopt($ch, CURLOPT_AUTOREFERER, TRUE);
+	    curl_setopt($ch, CURLOPT_HEADER, 0);
+	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	    curl_setopt($ch, CURLOPT_URL, $url);
+	    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);       
+	    $data = curl_exec($ch);
+	    curl_close($ch);
+	    return $data;
 	}
 
 	public function getStations($params = null) {
